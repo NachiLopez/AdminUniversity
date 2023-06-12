1f71875e862734d5fba5043520579081395702b7
NachiLopez <nazarenolopez35@gmail.com>
2023-06-12 19:09:32 -0300

    StudentController:  Modify the generateReportStudent method for save the pdf in the documents automatically when create it. TeacherController: Add the generateReportStudent method. CourseController: Add the generateCourseReport method. InterfaceStudent and InterfaceTeacher: Add some input parameters. Student: Initialize the Set<StudentCourse>. Add the getSpecificCourse method. Create the StudentCourse class to relation the student with a specific course. XML: Add the dependency IText to generate the PDF's with the function generateReport.

--- src/main/java/com/AdminUniversity/Controller/AdminController.java
+++ src/main/java/com/AdminUniversity/Controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.AdminUniversity.Controller;
 
 import com.AdminUniversity.DTO.*;
+import com.AdminUniversity.repository.Repositories;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -8,8 +9,13 @@ import java.util.Scanner;
 public class AdminController {
 
     public void addStudent(Student student) {
-
+        if(!Repositories.getInstance().getStudentRepository().getDB().contains(student)){
+            Repositories.getInstance().getStudentRepository().save(student);
+        } else {
+            System.out.println("This student already is in the database.");
+        }
     }
+
 
 
     public void updateStudent(Student student) {
@@ -33,14 +39,16 @@ public class AdminController {
 
 
     public void deleteTeacher(Teacher teacher) {
-
+        if (Repositories.getInstance().getTeacherRepository().getDB().contains(teacher)) {
+            Repositories.getInstance().getTeacherRepository().delete(teacher);
+        } else {
+            System.out.println("This teacher already is not in the database");
+        }
     }
-
 
     public void addCourse(Course course) {
 
     }
-
 
     public void updateCourse(Course course) {
 
--- src/main/java/com/AdminUniversity/Controller/CourseController.java
+++ src/main/java/com/AdminUniversity/Controller/CourseController.java
@@ -1,15 +1,52 @@
 package com.AdminUniversity.Controller;
 
 import com.AdminUniversity.DAO.InterfaceCourse;
+import com.AdminUniversity.DTO.Course;
 import com.AdminUniversity.DTO.Student;
+import com.AdminUniversity.DTO.StudentCourse;
 import com.AdminUniversity.DTO.Teacher;
+import com.AdminUniversity.repository.Repositories;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class CourseController implements InterfaceCourse {
 
 
     @Override
-    public void generateCourseReport() {
+    public void generateCourseReport(Course course) {
+        Document document = new Document();
+        String fileName = ("course_report_idCourse" + course.getId() + ".pdf");
+        String destiny = System.getProperty("user.home") + File.separator + "Documents" + File.separator + fileName;
 
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(destiny));
+            document.open();
+
+            document.add(new Paragraph("Course Report"));
+            document.add(new Paragraph("ID Course: " + course.getId()));
+            document.add(new Paragraph("Name: " + course.getName()));
+            document.add(new Paragraph("Teacher: " + course.getTeacher()));
+            document.add(new Paragraph("Student list of this course:"));
+            for (Student students : course.getStudents()){
+                document.add(new Paragraph("-------------------------------------"));
+                document.add(new Paragraph("Name: " + students.getFirstName() + " " + students.getLastName()));
+                document.add(new Paragraph("User: " + students.getUser()));
+                document.add(new Paragraph("Email: "+ students.getEmail()));
+            }
+            document.add(new Paragraph("-------------------------------------"));
+
+            document.close();
+            System.out.println("Course report generated successfully.");
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
